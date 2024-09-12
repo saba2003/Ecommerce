6fbcebb4625459cabe6365962434f14c4343c5a5
saba2003 <sabamagala405@gmail.com>
2024-09-12 18:15:22 +0400

    changed backend domain

--- the project/the_frontend/src/graphql/queries.js
+++ the project/the_frontend/src/graphql/queries.js
@@ -1,4 +1,4 @@
-export const backend_url = "http://php-backend.infinityfreeapp.com/graphql"
+export const backend_url = "https://green-dragonfly-487865.hostingersite.com/"
 
 export const GET_ALL_PRODUCTS = `
   query {
