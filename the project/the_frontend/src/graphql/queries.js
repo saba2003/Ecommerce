2acbe71a34bc894f21fa5ed34937ecf9b2bac695
saba2003 <sabamagala405@gmail.com>
2024-09-12 16:56:50 +0400

    changed backend_url

--- the project/the_frontend/src/graphql/queries.js
+++ the project/the_frontend/src/graphql/queries.js
@@ -1,4 +1,4 @@
-export const backend_url = "http://php-backend.infinityfreeapp.com/"
+export const backend_url = "http://php-backend.infinityfreeapp.com/graphql"
 
 export const GET_ALL_PRODUCTS = `
   query {
